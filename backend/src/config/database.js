d184fcb769fb37247dbb5c3fd33a2e62adf742c9
dileepkumar-mulagada <162285455+dileepkumar-mulagada@users.noreply.github.com>
2024-07-05 21:36:56 +0530

    Delete backend directory

--- backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-
-const connectDB = mongoose.connect(process.env.mongoUrl).then(()=>{
-    console.log('connection established');
-}).catch(err => console.log(err))
-
-module.exports = connectDB
--- backend/src/responseHandler/responseHandler.js
+++
@@ -1,9 +0,0 @@
-const ResponseHandler = (res, data, message , status) => {
-    return res.status(status).json({
-      message,
-      data,
-    });
-  };
-  
-
-  module.exports={ResponseHandler}
